3bd467dc60db91cbf8382b7ed198eeed5f568ead
puntodamar <punto@privyid.tech>
2025-07-19 10:00:36 +0700

    add: not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-  <h2>Not Found</h2>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>Maybe check out all of our <router-link to="/coaches">coaches</router-link></p>
+    </base-card>
+  </section>
 </template>
